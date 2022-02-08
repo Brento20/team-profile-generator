ae047ed6c5d55b407636ac14cccfb67fe4e20bfb
Brenton Weaver <brenton.weaver@gmail.com>
2022-02-08 16:42:52 +1100

    worked out that i needed to add jest to my package.json and i got the testing working. tests passed for the employee.js

--- _tests_/employee.test.js
+++ _tests_/employee.test.js
@@ -1,15 +1,15 @@
 const Employee = require('../lib/employee.js')
-
-const { expect } = require("@jest/globals");
+// const { expect } = require("@jest/globals");
 
 describe('Employee class', () => {
     describe('Create all staff', () => {
         it('A new object is created with a name and ID', () => {
-            const employee = new Employee(01, "Brenton", "brenton.weaver@gmail.com")
+            const employee = new Employee("01", "Brenton", "brenton.weaver@gmail.com")
 
-            expect(employee.id).toEqual(01);
+            expect(employee.id).toEqual("01");
             expect(employee.name).toEqual("Brenton");
             expect(employee.email).toEqual("brenton.weaver@gmail.com");
+            console.log(employee);
         });
     });
 });
