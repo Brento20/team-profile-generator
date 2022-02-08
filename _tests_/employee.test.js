d0d7daf2cc36556da85c271c3763eb09ef7985d2
Brenton Weaver <brenton.weaver@gmail.com>
2022-02-08 15:40:23 +1100

    worked on the employee.js then got stuck getting jest to work lol

--- _tests_/employee.test.js
+++ _tests_/employee.test.js
@@ -1,12 +1,15 @@
-// const Staff = require(location of allStaff.js)
+const Employee = require('../lib/employee.js')
+
+const { expect } = require("@jest/globals");
 
 describe('Employee class', () => {
     describe('Create all staff', () => {
         it('A new object is created with a name and ID', () => {
-            const employee = new Employee(01, "Brenton")
+            const employee = new Employee(01, "Brenton", "brenton.weaver@gmail.com")
 
             expect(employee.id).toEqual(01);
             expect(employee.name).toEqual("Brenton");
+            expect(employee.email).toEqual("brenton.weaver@gmail.com");
         });
     });
 });
